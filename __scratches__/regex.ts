3d459f774166c35ff238f7b08f24521afe44525e
Ivan <zhoga.ivan@gmail.com>
2019-09-05 04:38:19 +1100

    regex

--- __scratches__/regex.ts
+++ __scratches__/regex.ts
@@ -40,3 +40,19 @@ const arr = obj
   .join('-')
 console.log(arr)
 
+
+const test = '12weord test'
+// Non-capture group:
+test.match(/(?:\d+)(\w+)/) //?
+// Positive lookahead: (one or more \d, followed by one or more \w)
+test.match(/(\d+)(?=\w+)/) /*?*/
+// Negative lookahead: matches only 1 digit, otherwise would had to return null (pattern shouldn't match)
+test.match(/(\d+)(?![a-z]+)/) /*?*/
+// Positive lookbehind: does not create a group; must have a fixed width!
+test.match(/(?<=\d{2})(\w*)/) /*?*/
+// Negative lookbehind: only foo that is not being preceded by 'not '; must have fixed width!
+const foo = 'not foo but foo'
+foo.match(/(?<!not )foo/) //?
+test.match(/(?<!\d{2})[a-z]+/gi) //?
+// only matches the one with gidits; can't figure out how to reverse
+test.match(/(\d+)[a-z]+/gi) //?
