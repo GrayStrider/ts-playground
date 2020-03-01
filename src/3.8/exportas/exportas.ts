b7cf2a1e75bf7195ca46d6921d170b1abfdb1b49
Gray Strider <zhoga.ivan@gmail.com>
2020-03-01 21:07:02 +1100

    feat: export * as

---
+++ src/3.8/exportas/exportas.ts
@@ -0,0 +1,4 @@
+import { everything } from "src/3.8/exportas/index";
+
+console.log(everything.foo)
+console.log(everything.bar)
---
+++ src/3.8/exportas/index.ts
@@ -0,0 +1,1 @@
+export * as everything from 'src/3.8/exportas/mylib'
