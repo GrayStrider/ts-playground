daf7503981214dd77a2d84b827f9e59e56b89579
Gray Strider <zhoga.ivan@gmail.com>
2020-03-02 02:44:03 +1100

    feat: hasOwnProperty utility func

--- src/_scratch3.ts
+++ src/_scratch3.ts
@@ -19,5 +19,25 @@ const str2: union = 'hello'
 
 const options: Record<union, string> = {
 	world: '',
-	hello: ''
+	hello: '',
+}
+
+
+let person = {}
+
+function hasOwnProperty<X extends {}, Y extends PropertyKey>
+(obj: X, prop: Y): obj is X & Record<Y, unknown> {
+	return obj.hasOwnProperty(prop)
+}
+
+// person is an object
+if (typeof person === 'object'
+	// person = { } & Record<'name', unknown>
+	// = { } & { name: 'unknown'}
+	&& hasOwnProperty(person, 'name')
+	// yes! name now exists in person 👍
+	&& typeof person.name === 'string'
+) {
+	console.log(person.name.toUpperCase())
+	// do something with person.name, which is a string
 }
