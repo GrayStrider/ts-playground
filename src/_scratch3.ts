b8f0bead0def9728039bc3980aec38e265e6a81f
Gray Strider <zhoga.ivan@gmail.com>
2020-03-02 01:46:13 +1100

    feat: enum vs union (union wins)

--- src/_scratch3.ts
+++ src/_scratch3.ts
@@ -1,1 +1,23 @@
 export {}
+
+enum test {
+	hello = 'hello',
+}
+
+type union =
+	'hello' | 'world'
+
+enum test2 {
+	world
+}
+
+console.log(test.hello)
+console.log(test2.world)
+
+const str: test.hello = test.hello
+const str2: union = 'hello'
+
+const options: Record<union, string> = {
+	world: '',
+	hello: ''
+}
