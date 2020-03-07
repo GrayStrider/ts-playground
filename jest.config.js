7dea49cd28a638f922728ce4bd609827805472df
Gray Strider <zhoga.ivan@gmail.com>
2020-03-07 21:38:38 +1100

    config: package name

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 const tsconfig = require('./tsconfig')
 const baseConfig = require('@strider/utils-ts/jest.config.base')
+const pkg = require('./package')
 
 module.exports = {
-	...baseConfig(tsconfig)
+	...baseConfig(tsconfig, pkg)
 }
