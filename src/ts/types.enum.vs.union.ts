aa8fcc8d7bf8932364eee61c3c3eef7da2539a0e
Gray Strider <zhoga.ivan@gmail.com>
2020-03-07 21:18:32 +1100

    types.enum.vs.union.ts

---
+++ src/ts/types.enum.vs.union.ts
@@ -0,0 +1,25 @@
+enum E2 {
+	hello = 'hello',
+	world = 'world',
+	another = 'another',
+}
+
+enum E {
+	hello,
+	world,
+	another,
+}
+
+type U =
+	'hello'
+	|
+	'world'
+	|
+	'another'
+
+type U2 =
+	'hello'
+	| 'world'
+	| 'another'
+	| 'value'
+	| 'this'
