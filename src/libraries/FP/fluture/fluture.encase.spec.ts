5f9d88ef05d46b97319ca7a2eb8166249a9cb5ae
Gray Strider <zhoga.ivan@gmail.com>
2020-03-09 04:35:05 +1100

    future to promise examples

--- src/libraries/FP/fluture/fluture.encase.spec.ts
+++ src/libraries/FP/fluture/fluture.encase.spec.ts
@@ -1,4 +1,4 @@
-import { mapRej, fork } from 'fluture'
+import { mapRej, fork, promise } from 'fluture'
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { map } from 'fp-ts-fluture/lib/Future'
 import { pipe } from 'fp-ts/lib/pipeable'
@@ -11,7 +11,6 @@ const axiosF = toTheFuture<AxiosError, AxiosResponse> (axios.get)
 const get = (url: string) =>
 	axiosF (url, { headers: '' })
 		.pipe (map (pipe (prop ('data'))))
-		.pipe (mapRej (rej => rej.code ?? 'Error'))
 
 describe ('axios', () => {
 	const base = 'http://www.example.com',
@@ -34,11 +33,26 @@ describe ('axios', () => {
 	
 	test ('passes when rejects', (done) => {
 		expect.assertions (1)
-		get ('***').pipe (fork
-			(rej => {
-				expect (rej).toStrictEqual ('ECONNREFUSED')
-				done ()
-			}) (done),
-		)
+		get ('***')
+			.pipe (mapRej (({ code }) => code ?? 'Error'))
+			.pipe (fork (rej => {
+					expect (rej).toStrictEqual ('ECONNREFUSED')
+					done ()
+				}) (done),
+			)
+	})
+	
+	test ('passes when promise resolves', async () => {
+		expect.assertions (1)
+		const res = promise (get (url))
+		expect (await res).toStrictEqual ('done')
+	})
+	
+	test ('passes when promise rejects', async () => {
+		expect.assertions (1)
+		const res = promise (get ('***'))
+		await expect (res).rejects.toMatchObject ({
+			code: 'ECONNREFUSED',
+		})
 	})
 })
