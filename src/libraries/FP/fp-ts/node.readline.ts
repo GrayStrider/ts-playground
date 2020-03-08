6d0627bd662feced77d199d48e5c25e8777bc238
Gray Strider <zhoga.ivan@gmail.com>
2020-03-09 03:56:39 +1100

    feat: Task, node readline

---
+++ src/libraries/FP/fp-ts/node.readline.ts
@@ -0,0 +1,21 @@
+import { Task } from 'fp-ts/lib/Task'
+import { createInterface } from 'readline'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { toUpper } from 'ramda'
+import { sig } from '@strider/utils-ts'
+
+const read: Task<string> = () =>
+	new Promise<string> (resolve => {
+		const rl = createInterface ({
+			input: process.stdin,
+			output: process.stdout,
+		})
+		rl.question ('Type here: ', answer => {
+			rl.close ()
+			resolve (answer)
+		})
+	})
+
+read()
+	.then(pipe(toUpper))
+	.then(sig.success)
