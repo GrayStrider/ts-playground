9f80caa304d129e00b19015bee2fbf07f8022b95
Gray Strider <zhoga.ivan@gmail.com>
2020-03-08 22:21:27 +1100

    axios

--- src/libraries/fluture.axios.ts
+++ src/libraries/fluture.axios.ts
@@ -3,6 +3,9 @@ import axios, { AxiosError, AxiosResponse } from 'axios'
 import { encaseP, promise, map } from 'fluture'
 import { prop } from 'fp-ts-ramda'
 
+const faxiosGet = encaseP<AxiosError, AxiosResponse, string>
+(axios.get)
+
 describe ('axios', () => {
 	const base = 'http://www.example.com',
 		path = '/resource', url = base + path
@@ -21,10 +24,9 @@ describe ('axios', () => {
 	it ('should invoke axios', async () => {
 		expect.assertions (1)
 		
-		const axiosFu = encaseP<AxiosError, AxiosResponse, string>
-		(axios.get)
+
 		const act = promise (
-			axiosFu (url).pipe (
+			faxiosGet (url).pipe (
 				map (prop ('data')),
 			),
 		)
