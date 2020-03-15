f116a2166d65c105ee86fd32cef5fc22a9586797
Gray Strider <zhoga.ivan@gmail.com>
2020-03-16 00:52:13 +1100

    del fluture experiments

--- src/types/Fluture.d.ts
+++
@@ -1,8 +0,0 @@
-import Future, { FutureInstance, ResolveFunction, RejectFunction } from 'fluture'
-
-declare global {
-	interface Promise<T> {
-		toFuture (rej: RejectFunction<T>, res: ResolveFunction<T>): FutureInstance<T, any>
-	}
-	
-}
--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Future, { RejectFunction, ResolveFunction, FutureInstance } from 'fluture'
-
-declare module 'fluture' {
-	export interface FutureTypeRep {
-		then (rej: RejectFunction<T>, res: ResolveFunction<T>): Promise<any>
-		hey(): boolean
-	}
-}
